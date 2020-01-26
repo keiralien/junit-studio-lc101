410df6348528379b7f359fc8748bdbf7f44a3dc2
keiralien <jen.murphy8200@gmail.com>
2020-01-25 20:14:43 -0600

    Studio class 5

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,6 +1,9 @@
 package main;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
@@ -23,13 +26,67 @@ public class BalancedBrackets {
      */
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
+        int bracketCount = 0;
+
         for (char ch : str.toCharArray()) {
+
             if (ch == '[') {
                 brackets++;
+                bracketCount++;
             } else if (ch == ']') {
                 brackets--;
+                bracketCount++;
             }
+
+            if (brackets < 0 || brackets > 1) {
+                return false;
+            }
+
         }
-        return brackets == 0;
+
+        if (bracketCount > 0) {
+            return brackets == 0;
+        } else {
+            return false;
+        }
+
     }
 }
+
+//    List<Integer> openIndexCheck = new ArrayList<>();
+//    List<Integer> closeIndexCheck = new ArrayList<>();
+//        for (char ch : str.toCharArray()) {
+//                if (ch == '[') {
+//                int i = 0;
+//                brackets++;
+//                bracketCount++;
+//                if (openIndexCheck.size() == 0) {
+//                openIndexCheck.add(str.indexOf(ch));
+//                } else {
+//                openIndexCheck.add(str.indexOf(ch, openIndexCheck.get(i) + 1));
+//                i++;
+//                }
+//                } else if (ch == ']') {
+//                int j = 0;
+//                brackets--;
+//                bracketCount++;
+//                if (closeIndexCheck.size() == 0) {
+//                closeIndexCheck.add(str.indexOf(ch));
+//                } else {
+//                closeIndexCheck.add(str.indexOf(ch, closeIndexCheck.get(j) + 1));
+//                j++;
+//                }
+//                }
+//                if (brackets < 0) {
+//        return false;
+//        } else if (brackets > 1) {
+//        return false;
+//        }
+//        }
+//        if (bracketCount > 0 && openIndexCheck.get(0) < closeIndexCheck.get(0)) {
+//        return brackets == 0;
+//        } else {
+//        return false;
+//        }
+//
+//        }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,79 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
-    @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    private static int testCount = 0;
+
+    @BeforeClass
+    public static void setup() {
+        System.out.println("Unit tests starting:");
     }
 
+    @After
+    public void message() {
+        System.out.println("Test " + ++testCount + " complete.");
+    }
+
+    @AfterClass
+    public static void closing() {
+        System.out.println("Total of " + testCount + " tests complete.");
+    }
+
+    @Test
+    public void onlyOrderedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void emptyStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void noBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{}"));
+    }
+
+    @Test
+    public void singleCloseBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void singleOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void mixedBalancedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[{}][][floof]"));
+    }
+
+    @Test
+    public void mixedUnbalancedOpenBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[bl[ue]"));
+    }
+
+    @Test
+    public void mixedUnbalancedCloseBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[bl]ue]"));
+    }
+
+    @Test
+    public void insetBalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[t[r]y]"));
+    }
+
+    @Test
+    public void unorderedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 
 }
